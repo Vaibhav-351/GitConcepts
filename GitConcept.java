8271e594be1a0f96ae0ccedc9baa7a477c75963b
Vaibhav <vaibhavmore039@gmail.com>
2025-10-11 10:08:52 +0530

    added printDuckmerge methd again

--- GitConcept.java
+++ GitConcept.java
@@ -7,11 +7,17 @@ public class GitConcepts
         System.out.println("git concepts");
         printPeacock();
         printDuck();
+        printDuckMerge();
     }
     //This method added to check if it reflects in new branch
     public static void printDuck()
     {
         System.out.println("Duck");
+    }
+//merge changes
+    public static void printDuckMerge()
+    {
+        System.out.println("DuckMerge");
     }
     //added
     public static void printPeacock()
