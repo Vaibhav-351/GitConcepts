828259b0f5d0f2310162ec03ef931a3477ce00a2
Vaibhav <vaibhavmore039@gmail.com>
2026-02-19 23:00:08 +0530

    added printDuck method again.

--- GitConcepts.java
+++ GitConcepts.java
@@ -4,7 +4,13 @@ public class GitConcepts
     public static void main(STrings[] args)
     {
         system.out.println("git Concepts");
+        printDuck();
         printPeacock();
+    }
+
+    public static void printDuck()
+    {
+        system.out.println("Duck");
     }
 
     public static void printPeacock()
