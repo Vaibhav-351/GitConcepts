5d2473f3392d8f84016c59f04a5b92c20e266618
Vaibhav <vaibhavmore039@gmail.com>
2026-03-03 20:27:49 +0530

    Adding Books

---
+++ BookPrinter.java
@@ -0,0 +1,14 @@
+public class BookPrinter
+{
+    //This is the main method
+    public static void main(STrings[] args)
+    {
+        printBooks();
+    }
+
+    public static void printBooks()
+    {
+        system.out.println("Printing Books-----");
+    }
+
+}
