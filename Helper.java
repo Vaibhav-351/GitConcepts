0849996bd4141f518fcada4becb0fcaef4eafe86
Rupraj <ruprajmore@gmail.com>
2026-03-03 20:24:50 +0530

    Improve Help message

--- Helper.java
+++ Helper.java
@@ -4,6 +4,7 @@ public class Helper
     public static void printHelp()
     {
         system.out.println("Help for Git Concept project");
+        system.out.println("This project is created for learning the conecpts.");
     }
 
 }
